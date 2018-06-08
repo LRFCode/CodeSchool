efabef94d0461c46b44619a2e66e3e01afc78930
Louis <lrflrf@sbcglobal.net>
2018-06-08 13:12:38 -0500

    start project

--- db/CodeSchool.sql
+++ db/CodeSchool.sql
@@ -6,14 +6,6 @@ CREATE TABLE Category (
 );
 
 
-CREATE TABLE Challenge (
-                ChallengeId INT AUTO_INCREMENT NOT NULL,
-                ChallengeName VARCHAR(50) NOT NULL,
-                CategoryId INT NOT NULL,
-                PRIMARY KEY (ChallengeId)
-);
-
-
 CREATE TABLE Language (
                 LanguageId INT AUTO_INCREMENT NOT NULL,
                 LanguageName VARCHAR(50) NOT NULL,
@@ -21,8 +13,23 @@ CREATE TABLE Language (
 );
 
 
+CREATE TABLE Challenge (
+                ChallengeId INT AUTO_INCREMENT NOT NULL,
+                ChallengeName VARCHAR(50) NOT NULL,
+                CategoryId INT NOT NULL,
+                LanguageId INT NOT NULL,
+                PRIMARY KEY (ChallengeId)
+);
+
+
 ALTER TABLE Challenge ADD CONSTRAINT category_challenge_fk
 FOREIGN KEY (CategoryId)
 REFERENCES Category (CategoryId)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE Challenge ADD CONSTRAINT language_challenge_fk
+FOREIGN KEY (LanguageId)
+REFERENCES Language (LanguageId)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
